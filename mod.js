f25405a732846974bda7130be29edc0db992493d
Wouter Visser <fusion86@cerbus.nl>
2018-12-27 11:27:02 +0100

    Screw this I give up

--- mod.js
+++ mod.js
@@ -1,25 +1,33 @@
 // Disable overscrolling/elastic scrolling
 window.parent.document.getElementsByTagName("html")[0].style.overflow = "hidden";
 
-// #region Patch mousewheel event
+// #region Override mousewheel event
 
-orig_func_name = 'n7a'; // TODO: Get dynamically
-orig = ig.input[orig_func_name].toString(); // Get original eventhandler code
-code = orig.substring(orig.indexOf('{') + 1, orig.lastIndexOf('}')); // Stip `function {` and `}`
-
-// Patch code so that it works better on macOS
-// TODO: ...
-
-// Create new function with patched code
-orig_func = new Function('a', code); // Argument name should always be `a` (because that is how the uglifier works)
+var prev_time = 0;
+var prev_delta = 0;
 
 // Override mousewheel event
 window.addEventListener("mousewheel", function (event) {
     event.stopPropagation(); // Stop the original mousewheel implementation from running
 
-    // Call original scroll function
-    // We are using orig_func.call() so that `this` points to `ig.input` (as expected by the original code)
-    orig_func.call(ig.input, event);
+    var cur_time = new Date().getTime();
+    var time_diff = cur_time - prev_time;
+    var delta = Math.abs(event.deltaY);
+
+    var is_touchpad = Number.isInteger(delta); // Touchpad always has non-decimal delta values, I think?
+
+    if (is_touchpad) {
+        // Ignore inertial scrolls, sorta?
+        if (time_diff < 100 && delta < 50) return;
+
+        // Ignore slow scrolls
+        if (delta < 3) return;
+    }
+
+    prev_time = cur_time;
+    prev_delta = delta;
+
+    ig.input.n7a.call(ig.input, event); // TODO: Get `n7a` name dynamically
 }, true);
 
 // #endregion
